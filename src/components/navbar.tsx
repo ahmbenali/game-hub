d70d88c70e7060121c78d25cfa489dbd743bb2c1
Ahmed Benali <abenali.dev@gmail.com>
2024-07-03 23:57:42 +0200

    Build the color mode switch

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
+import ThemeToggler from './them-toggler'
 
 function Navbar() {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' p='10px'>
       <Image boxSize='60px' src={logo} alt="logo" />
-      <Text>Navbar</Text>
+      <ThemeToggler />
     </HStack>
   )
 }
---
+++ src/components/them-toggler.tsx
@@ -0,0 +1,18 @@
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+
+function ThemeToggler() {
+  const { colorMode: theme, toggleColorMode: toggleTheme } = useColorMode()
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme='green'
+        isChecked={theme === 'dark'}
+        onChange={toggleTheme}
+      />
+      <Text>Dark Mode</Text>
+    </HStack>
+  )
+}
+
+export default ThemeToggler
